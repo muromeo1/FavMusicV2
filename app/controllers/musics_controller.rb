59864d12dc54ce087509b4fbb5d74ed69cd56c48
Murilo Hernandes Romeo <muriloh65@gmail.com>
2018-12-15 19:18:35 -0200

    Search by title or genre was made

--- app/controllers/musics_controller.rb
+++ app/controllers/musics_controller.rb
@@ -1,2 +1,5 @@
 class MusicsController < ApplicationController
+  def index
+    @music = Music.all
+  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,7 +8,7 @@ class UsersController < ApplicationController
 
     if @user.save
       flash[:notice] = "Dados salvos com sucesso!"
-      redirect_to users_path
+      redirect_to musics_path
     else
       flash[:danger] = "Algo deu errado..."
       redirect_to new_user_path
