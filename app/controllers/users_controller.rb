72d85005cd38b624a7a7e8ff0000964f9f81f987
Murilo Hernandes Romeo <muriloh65@gmail.com>
2018-12-14 23:49:43 -0200

    Register component working

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,24 @@
 class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.create(users_params)
+
+    if @user.save
+      flash[:notice] = "Dados salvos com sucesso!"
+      redirect_to users_path
+    else
+      flash[:error] = "Algo deu errado..."
+      redirect_to new_user_path
+    end
+  end
+
+  # TODO: Don't accept duplicate emails
+
+  private
+  def users_params
+    params.require(:user).permit(:name, :email, :age)
+  end
 end
